dd0d86d38980ce6366db24c631c367bab1ee67dd
david100gom <david100gom@gmail.com>
2017-10-25 00:43:05 +0900

    server system-sleep time add, Web UI modify

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
+var sleep = require('system-sleep');
+
+app.use(express.static('static'));
 app.use(bodyParser.json());
 
 app.get('/',function(req, res){  //2
@@ -11,28 +14,28 @@ app.get('/',function(req, res){  //2
 });
 
 app.post('/beacon',function(req, res){  //2
-    io.emit("receive message", 'SUUID : '+req.body.SUUID+', 위도 : '+req.body.lat+', 경도 :'+req.body.lng);
-    io.emit("receive message", "----------------------------------------------------------------------------------------------------------");
+    io.emit("receive message", 'SUUID : '+req.body.SUUID+', 위도 : '+req.body.lat+', 경도 :'+req.body.lng+'\n\n');
+    sleep(1*1000); // sleep for 1 seconds
     res.send({ "result" : 1});
 });
 
-var count=1;
-io.on('connection', function(socket){ //3
-    console.log('user connected: ', socket.id);  //3-1
-    var name = "user" + count++;                 //3-1
-    io.to(socket.id).emit('change name',name);   //3-1
-
-    socket.on('disconnect', function(){ //3-2
-        console.log('user disconnected: ', socket.id);
-    });
-
-    socket.on('send message', function(name,text){ //3-3
-        var msg = name + ' : ' + text;
-        console.log(msg);
-        io.emit('receive message', msg);
-    });
-});
+// var count=1;
+// io.on('connection', function(socket){ //3
+//     console.log('user connected: ', socket.id);  //3-1
+//     var name = "user" + count++;                 //3-1
+//     io.to(socket.id).emit('change name',name);   //3-1
+//
+//     socket.on('disconnect', function(){ //3-2
+//         console.log('user disconnected: ', socket.id);
+//     });
+//
+//     socket.on('send message', function(name,text){ //3-3
+//         var msg = name + ' : ' + text;
+//         console.log(msg);
+//         io.emit('receive message', msg);
+//     });
+// });
 
 http.listen(3000, function(){ //4
-    console.log('server on!');
+    console.log('Server Start.');
 });
